154908792c91e9922f15602d592d557443b64444
YinnisHome <kempeneers.yinnis@gmail.com>
2025-11-28 00:15:07 +0100

    2

--- Convert_IM_B64.h
+++ Convert_IM_B64.h
@@ -7,14 +7,22 @@
 #include "stdlib.h"
 #include "stdint.h"
 
-#define MAX_INPUT_SIZE	8192
+#define MAX_BUFFER_SIZE	8192
+#define MAX_USER_INPUT  50
 
 typedef struct PNG_HEADER
 {
     char png_sign[4];
     char IHDR_verify[5];
+    uint32_t chunkdata;
     uint32_t width;
     uint32_t height;
+    uint32_t totalSize;
+    uint8_t colorByteSize;
+    uint8_t colorType;
+    uint8_t compression;
+    uint8_t filter;
+    uint8_t enlacement;
 }png_header_t;
 
 uint32_t covert_bigEndian(uint8_t *data,int offset);
--- main.c
+++ main.c
@@ -8,37 +8,34 @@
 int main()
 {
     png_header_t png_header;
-    char in_buffer[50];
-    unsigned char in_file[MAX_INPUT_SIZE];
-
+    char in_buffer[MAX_USER_INPUT];
+    unsigned char in_file[MAX_BUFFER_SIZE];
     FILE *fileptr;
 
     // Ask file and open.
-    fgets(in_buffer,50,stdin);
+    fgets(in_buffer,MAX_USER_INPUT,stdin);
     in_buffer[strcspn(in_buffer,"\n")] = '\0';
     fileptr = fopen(in_buffer,"rb");
 
     // Read binary data into file buffer
-    fread(in_file,1,MAX_INPUT_SIZE,fileptr);
+    fread(in_file,1,MAX_BUFFER_SIZE,fileptr);
 
     // PNG signature first 8 bytes (0-7)
     strncpy_s(png_header.png_sign,4,(char*)in_file+1,3); // first char is garbage PNG start at byte 1 not 0
-    printf("%s\n",png_header.png_sign);
-
-    // length starts at byte 8 (8-11)
-    // Don't need this
 
     // IHDR starts at byte 12 (12-15)
     strncpy_s(png_header.IHDR_verify,5,(char*)in_file+12,4);
-    printf("%s\n",png_header.IHDR_verify);
 
     // Width starts at byte 16 (16-19)
     png_header.width = covert_bigEndian(in_file,16);
-    printf("%u\n",png_header.width);
 
     // Height starts at byte 20 (20-23)
     png_header.height = covert_bigEndian(in_file,20);
-    printf("%u\n",png_header.height);
+
+    // Print
+    printf(" %s\n %s\n Width %u\n Height : %u\n",
+        png_header.png_sign,png_header.IHDR_verify,
+        png_header.width,png_header.height);
 
     fclose(fileptr);
     return 0;
